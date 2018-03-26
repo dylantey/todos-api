fa4542d74d9ed0d0c779ef25431960f8c28c4188
Dylan Tey <cheeho_97@outlook.com>
2018-03-26 03:36:48 +0000

    second push

--- api/models/todosModel.js
+++ api/models/todosModel.js
@@ -2,22 +2,59 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
-var taskSchema = new schema({
-  name: {
+var form = new schema({
+  deliveryId: {
+    type: Number,
+    Required: 'A delivery id must be specified'
+  },
+  storeId: {
+    type: Number,
+    Required: 'A store id must be specified'
+  },
+  restaurantName: {
     type: String,
-    Required: 'A task name is required'
+    Required: 'A restaurant name is required'
   },
-  created: {
-    type: Date,
-    default: Date.now
+  logoUrl: {
+    type: String,
+    Required: 'A logo url is required'
   },
-  status: {
-    type: [{
-      type: String,
-      enum: ['not started', 'in-progress', 'completed']
-    }],
-    default: ['not started']
+  cutoff: {
+    type: String,
+    Required: 'A cutoff time is required'
+  },
+  dropoff: {
+    type: String,
+    Required: 'A dropoff time is required'
+  },
+  soldOut: {
+    type: Boolean,
+    Required: 'A boolean for soldOut is requried'
+  },
+  sellingOut: {
+    type: Boolean,
+    Required: 'A boolean for sellingOut is requried'
+  },
+  isPastCutoff: {
+    type: Boolean,
+    Required: 'A boolean for isPastCutoff is requried'
+  },
+  isOrderPlaced: {
+    type: Boolean,
+    Required: 'A boolean for isOrderPlaced is requried'
   }
+});
+
+var delivery_day = new schema ({
+  day: {
+    type: String,
+    Required: 'Day of Delivery is required'
+  },
+  deliveries: [form]
+});
+
+var taskSchema = new schema ({
+  dropoffs: [delivery_day]
 });
 
 module.exports = mongoose.model('Tasks', taskSchema);
